6a97288c1c892fdcfaa8259824b772bbff27cd73
kotaro <trevorfavel882@gmail.com>
2025-12-02 16:20:05 -0800

    cart quantity

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,29 +1,7 @@
-const products = [{
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090
-    },{
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095
-    },{
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        priceCents: 799
-    }
-];
+// const products = [{
+//         // ... see products.js
+//     }
+// ];
 
 let productsHTML = '';
 products.forEach((product) => {
@@ -72,7 +50,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button js-add-to-cart button-primary" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -80,3 +58,25 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-gird').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.push({productId, quantity: 1});
+        }
+
+        
+        console.log(cart);
+    });
+});
